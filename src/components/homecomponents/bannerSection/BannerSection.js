15fbaa2570d6bf1ed0d2d8e1fb06ca9972c39b8d
yashrajput0001 <yash.technogetic@gmail.com>
2024-01-11 11:20:37 +0530

    all changes are done

--- src/components/homecomponents/bannerSection/BannerSection.js
+++ src/components/homecomponents/bannerSection/BannerSection.js
@@ -4,7 +4,7 @@ import img from "../../../assets/images/pexels-pixabay-534220.jpg"
 function BannerSection({heading,para}) {
   return (
     <>
-      <div className='backgroundimage' style={{background:`url(${img})`,height:"100vh",backgroundPosition:"center",backgroundSize:"cover",backgroundRepeat:"no-repeat"}}>
+      <div className='backgroundimage' style={{backgroundImage:`url(${img})`}}>
       <div className='bannerbox firstbannerbox container'>
             <h2 className='bannerHeading'>{heading}</h2>
             <p className='bannerPara'>{para}</p>
